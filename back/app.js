5d1bdef8c6ad7bfee0c072e95a537e3060025cc2
devfloors <csh47790@gmail.com>
2020-10-30 01:44:11 +0900

    3nd commit

--- back/app.js
+++ back/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
 const routers = require('./routes');
 const db = require('./models');
 
@@ -37,6 +40,14 @@ db.sequelize.sync()
         console.log('db 연결 성공 ')
     })
     .catch(console.error);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(hpp());
+    app.use(helmet());
+} else {
+    app.use(morgan('dev'));
+}
 
 app.use('/', routers);
 app.use(function (err, req, res, next) {
