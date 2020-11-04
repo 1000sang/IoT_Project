3f51417c2fccd8ba04336799536fe1f0cf3d556f
devfloors <csh47790@gmail.com>
2020-11-05 01:55:24 +0900

    1105

--- front/next.config.js
+++ front/next.config.js
@@ -9,7 +9,8 @@ const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 // next.js custom configuration goes here
 const nextConfig = {
   env: {
-    BACKEND_URL: 'http://localhost:3065',
+    BACKEND_URL: 'http://ec2.devfloors.com:3065',
+    // 'http://localhost:3065',
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
