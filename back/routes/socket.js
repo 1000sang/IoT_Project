9c8bbd7ef2584a6f59463d56777c14f327603e67
devfloors <csh47790@gmail.com>
2020-11-19 00:35:16 +0900

    1119

--- back/routes/socket.js
+++ back/routes/socket.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('../utils/passport/confirmLogin');
 const socketController = require('../controllers/socket');
 
-router.get('socket', isLoggedIn, socketController.getSocket)
+router.get('/', isLoggedIn, socketController.getSocket)
 router.post('/:userId', isLoggedIn, socketController.createSocketRoom);
 
 module.exports = router;
