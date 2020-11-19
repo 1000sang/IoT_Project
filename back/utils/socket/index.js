982da9bca0d2a675131ac04bbbaef6d387a0bdac
devfloors <csh47790@gmail.com>
2020-11-20 04:09:11 +0900

    1120

--- back/utils/socket/index.js
+++ back/utils/socket/index.js
@@ -12,10 +12,6 @@ const mqttOptions = {
     protocol: 'mqtt'
 }
 
-// client.on('message', function (topic, message) {
-//     console.log(`토픽:${topic.toString()},메세지: ${message.toString()}`)
-// })
-
 module.exports = (server, app) => {
     const io = socketIo(server, { origin: '*:*' });
     const mqttClient = mqtt.connect(mqttOptions);
@@ -31,6 +27,10 @@ module.exports = (server, app) => {
         console.log(err)
     })
 
+    mqttClient.on('message', function (topic, message) {
+        console.log(`토픽:${topic.toString()},메세지: ${message.toString()}`)
+    })
+
     deviceRoom.on('connection', (socket) => {
         console.log('device 네임스페이스 접속');
         let devices = null;
@@ -39,28 +39,33 @@ module.exports = (server, app) => {
         socket.on('login', (data) => {
             console.log('login data', data)
 
-            // redisClient.hgetall(`${data.userId}/device`, (err, obj) => {
-            //     devices = Object.keys(obj);
-            //     topics = Object.values(obj);
+            redisClient.hgetall(`${data.userId}/device`, (err, obj) => {
+                if (err) {
+                    console.log('hgetall err', err)
+                    return;
+                } else {
+                    // devices = Object.keys(obj);
+                    // topics = Object.values(obj);
 
-            //     devices.map((v) => {
-            //         socket.join(v);
-            //         console.log(`${v} 룸이 생성되었습니다.`);
-            //         socket.to(`${v}`).emit('join', {
-            //             msg: `${v}루에 입장하셧습니다.`
-            //         })
-            //     })
+                    // devices.map((v) => {
+                    //     socket.join(v);
+                    //     console.log(`${v} 룸이 생성되었습니다.`);
+                    //     socket.to(`${v}`).emit('join', {
+                    //         msg: `${v}루에 입장하셧습니다.`
+                    //     })
+                    // })
 
-            //     topics.map((v, i) => {
-            //         console.log(v)
-            //         // mqttClient.subscribe(v);
+                    // topics.map((v, i) => {
+                    //     console.log(v)
+                    //     // mqttClient.subscribe(v);
 
-            //     })
+                    // })
 
-            //     // console.log('redisClient', Object.values(obj))
-            //     // console.log('redisClient', Object.values(obj)[0])
-            //     // console.log('redisClient deviceId', obj[0].deviceId)
-            // })
+                    // console.log('redisClient', Object.values(obj))
+                    // console.log('redisClient', Object.values(obj)[0])
+                    // console.log('redisClient deviceId', obj[0].deviceId)
+                }
+            })
         })
 
         socket.on('disconnect', () => {
