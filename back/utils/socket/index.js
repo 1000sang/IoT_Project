3c43b95c8725ef3cfc50c1d4b1e93c9e4ad5367d
devfloors <csh47790@gmail.com>
2020-11-19 18:31:56 +0900

    1119

--- back/utils/socket/index.js
+++ back/utils/socket/index.js
@@ -18,12 +18,13 @@ module.exports = (server, app, sessionMiddleware) => {
     device.on('connection', (socket) => {
         console.log('device 네임스페이스 접속');
         const req = socket.request;
-        const { headers: { referer } } = req;
+        // const { headers: { referer } } = req;
         // const roomId = referer
         //     .split('/')[referer.split('/'.length - 1)].
         //     replace(/\?.+/, '');
-        console.log('referer', referer)
-        console.log('roomId', roomId);
+        // console.log('referer', referer)
+        // console.log('roomId', roomId);
+        console.log('req', req)
         console.log('req.session', req.session)
         socket.on('disconnect', () => {
             console.log('device 네임스페이스 접속 해제');
