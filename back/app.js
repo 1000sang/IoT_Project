3fd72b99263459790cd478c7f268d93ea14397a0
devfloors <csh47790@gmail.com>
2020-11-22 23:34:08 +0900

    1121

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ const redisClient = require('./utils/redis');
 
 const routers = require('./routes');
 const db = require('./models');
-const mongoDB = require('./utils/mongo');
+const mongoDB = require('./models/mongo');
 const passportConfig = require('./utils/passport');
 const errMsg = require('./utils/error/errorMessage');
 const webSocket = require('./utils/socket');
--- back/models/mongo/index.js
+++ back/models/mongo/index.js
@@ -6,24 +6,27 @@ dotenv.config()
 
 module.exports = () => {
     const connect = () => {
-        mongoose.connect(process.env.MONGO_HOST, (err) => {
+        mongoose.connect(process.env.MONGO_HOST, {
+            useCreateIndex: true,
+            useNewUrlParser: true
+        }, (err) => {
             if (err) {
                 console.log(err)
             }
             console.log('mongodb connected')
         })
-
-        connect();
-
-        mongoose.connection.on('error', (err) => {
-            console.log('몽고디비 연결 에러', err);
-        });
-
-        mongoose.connection.on('disconnected', () => {
-            console.log('몽고디비 연결이 끊겼습니다. 재연결 시도합니다.')
-            connect();
-        });
     }
+
+    connect();
+
+    mongoose.connection.on('error', (err) => {
+        console.log('몽고디비 연결 에러', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('몽고디비 연결이 끊겼습니다. 재연결 시도합니다.')
+        connect();
+    });
 
     require('./room');
 
