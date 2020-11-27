342a7b425b7f8a7ca6b6345d8a0f7d7a4b0be23b
devfloors <csh47790@gmail.com>
2020-11-27 23:57:33 +0900

    1127

--- back/models/mongo/device.js
+++ back/models/mongo/device.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const deviceSchema = new mongoose.Schema({
     deviceId: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: string,
         required: true
     },
     topic: {
