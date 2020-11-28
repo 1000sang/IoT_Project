c9a29b973477401e20d7649f466d58926f40f011
devfloors <csh47790@gmail.com>
2020-11-28 16:01:16 +0900

    1128

--- back/models/mongo/sensorData.js
+++ back/models/mongo/sensorData.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
 const sensorDataSchema = new mongoose.Schema({
-    deviceId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Device',
-        required: true
-    },
     topic: {
         type: String,
-        required: true,
     },
     data: {
         type: String
