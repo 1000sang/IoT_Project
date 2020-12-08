bba6b1ddd25f0acb06a2f0b23e04eb3fa93923ab
devfloors <csh47790@gmail.com>
2020-12-09 01:53:48 +0900

    1208

--- back/routes/user.js
+++ back/routes/user.js
@@ -8,6 +8,6 @@ router.get('/', userController.getUser);
 router.get('/session', userController.getSesssion);
 router.post('/', isNotLoggedIn, userController.createUser);
 router.post('/login', isNotLoggedIn, userController.login);
-router.post('/logout', isLoggedIn, userController.logout);
+router.post('/logout', userController.logout);
 
 module.exports = router;
